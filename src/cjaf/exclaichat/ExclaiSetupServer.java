dae845d197c6623dbefff0c8b25ae6ff6551cb98
Delu Anachebe <cjaf.javacakess@gmail.com>
2013-08-30 16:57:29 +0100

    Some extra unimportant stuff

--- src/cjaf/exclaichat/ExclaiSetupServer.java
+++ src/cjaf/exclaichat/ExclaiSetupServer.java
@@ -14,61 +14,66 @@ import javax.swing.JTextField;
 
 public class ExclaiSetupServer extends JFrame{
 	ExclaiGUI eg;
-	
+
 	JLabel port;
 	JTextField portText;
-	
+
 	JLabel name;
 	JTextField nameText;
-	
+
 	JButton go;
 	JPanel pane;
 
 	public ExclaiSetupServer(ExclaiGUI eg) {
 		super("Host Server");
 		this.eg = eg;
-		
+
 		setUpGUI();
 		setUpWindowProperties();
-		
+
 	}
-	
+
 	public void setUpGUI() {
 		pane = new JPanel();
 		pane.setLayout(new FlowLayout(FlowLayout.CENTER));
-		
+
 		port = new JLabel("Port:");
 		pane.add(port);
-		
+
 		portText = new JTextField("5556");
 		pane.add(portText);
-		
+
 		name = new JLabel(" Name:");
 		pane.add(name);
-		
+
 		nameText = new JTextField("My awesome server");
 		pane.add(nameText);
-		
+
 		go = new JButton("Host");
 		pane.add(go, BorderLayout.PAGE_END);
 		add(pane, BorderLayout.NORTH);
-		
-		
+
+
 		go.addActionListener(
 			new ActionListener() {
 				public void actionPerformed(ActionEvent ae) {
-					int port = 0;
-					if (Util.canParseInt(portText.getText())) {
-						 Integer.parseInt(portText.getText());
-					} else {
-						JOptionPane.showMessageDialog(null, "Invalid port", "Error", JOptionPane.ERROR_MESSAGE);
-						return;
-					}
-					String serverName = nameText.getText();
-					if (serverName.isEmpty()) {
-						 java.awt.Toolkit.getDefaultToolkit().beep();
-						 return;
-					}
+						int port = 0;
+						if (Util.canParseInt(portText.getText())) {
+							int i = Integer.parseInt(portText.getText());
+							if (i < 80 || i > 65535) {
+								JOptionPane.showMessageDialog(null, "Invalid port number!"
+										+" Must be between 80 and 65535", "Error", JOptionPane.ERROR_MESSAGE);
+								return;
+							}
+						} else {
+							JOptionPane.showMessageDialog(null, "Invalid port number.", "Error", JOptionPane.ERROR_MESSAGE);
+							return;
+						}
+						String serverName = nameText.getText();
+						if (serverName.isEmpty()) {
+							java.awt.Toolkit.getDefaultToolkit().beep();
+							return;
+						}
 					hostServer(port, serverName);
 				}
 			}
